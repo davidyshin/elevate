2b1d8eacecc3c0ab730e47295ccc01257447142b
ramborell23 <jerelldavis@ac.c4q.nyc>
2018-03-16 16:51:36 -0400

    Update sql and quieries| Fixed table columns to avoid redundancies

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -2,6 +2,7 @@ const pgp = require('pg-promise')({});
 
 const db = pgp("postgres://localhost/capTest");
 
+//Users
 function getAllUsers() {
     return db.any('select * from Users')
 }
@@ -9,7 +10,7 @@ function getSingleUser(id) {
     return db.any('SELECT *  FROM Users WHERE id=$1',[id])
 }
 
-
+// Resume and Cover Letter info
 function getAllUsersInfo() {
     return db.any('select * from Users_Personal_Info')
 }
@@ -17,7 +18,7 @@ function getSingleUsersInfo(id) {
     return db.any('SELECT *  FROM Users_Personal_Info WHERE id=$1',[id])
 }
 
-
+//Resumes 
 function getAllResumes() {
     return db.any('select * from resumes')
 }
@@ -25,7 +26,7 @@ function getUsersResumes(id) {
     return db.any('SELECT *  FROM resumes WHERE id=$1',[id])
 }
 
-
+//Job Applied to
 function getAllJobApps() {
     return db.any('select * from jobs')
 }
@@ -35,5 +36,8 @@ function getSingleUserJobApps(id) {
 
 
 module.exports = {
-
+    getAllUsers,
+    getSingleUser,
+    getAllUsersInfo,
+    getSingleUsersInfo,
 }
