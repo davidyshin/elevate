3a25eb294ec88087389c832fd9dd7d120d2b3812
davidyshin <davidshin@ac.c4q.nyc>
2018-03-16 21:55:55 -0400

    enclosed all inputs in a form to keep code organized

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -15,7 +15,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      activeUser: 'mockUser'
+      activeUser: mockUser
     };
   }
 
--- frontend/src/components/jobs/AddJobForm.jsx
+++ frontend/src/components/jobs/AddJobForm.jsx
@@ -12,31 +12,40 @@ class AddJobForm extends Component {
     return (
       <div className="add-job-form">
         <div className="add-job-info">
-          <p>Company</p>
-          <input type="text" />
-          <p>Position</p>
-          <input type="text" />
-          <p>Date Applied</p>
-          <input type="date"  />
-          <p>Job Posting Url</p>
-          <input type="text" />
+          <h3> Job Info</h3>
+          <form>
+            <p>Company</p>
+            <input type="text" />
+            <p>Position</p>
+            <input type="text" />
+            <p>Date Applied</p>
+            <input type="date" />
+            <p>Job Posting Url</p>
+            <input type="text" />
+          </form>
         </div>
         <div className="add-job-resume">
-          <p>Resume</p>
-          <input type="file" />
+          <h3>Resume</h3>
+          <form>
+            <input type="file" />
+          </form>
         </div>
         <div className="add-job-coverletter">
-          <p>Cover Letter</p>
-          <input type="file" />
+          <h3>Cover Letter</h3>
+          <form>
+            <input type="file" />
+          </form>
         </div>
         <div className="add-job-interview">
           <h3>Interview 1</h3>
-          <p>Date</p>
-          <input type="date" />
-          <p>Time</p>
-          <input type="time" />
-          <p>Contact</p>
-          <input type="text" />
+          <form>
+            <p>Date</p>
+            <input type="date" />
+            <p>Time</p>
+            <input type="time" />
+            <p>Contact</p>
+            <input type="text" />
+          </form>
         </div>
       </div>
     );
