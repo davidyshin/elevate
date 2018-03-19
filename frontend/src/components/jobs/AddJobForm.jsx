bc289e9a1ccc671020ce84d05ad1c6a70b1c3411
davidyshin <davidshin@ac.c4q.nyc>
2018-03-19 17:55:33 -0400

    in-progress form for add a job to the database

--- frontend/src/components/jobs/AddJobForm.jsx
+++ frontend/src/components/jobs/AddJobForm.jsx
@@ -5,39 +5,102 @@ import React, { Component } from 'react';
 class AddJobForm extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      company: '',
+      position: '',
+      date: '',
+      url: ''
+    };
   }
-  submitForm = () => {};
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state)
+  };
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state);
+  };
+
+  handleDate = e => {
+   this.setState({date: e.target.value})
+  };
+
   render() {
+    const { company, position, date, url } = this.state;
     return (
       <div className="add-job-form">
         <div className="add-job-info">
           <h3> Job Info</h3>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <p>Company</p>
-            <input type="text" />
+            <input
+              onChange={this.handleInput}
+              value={company}
+              name="company"
+              type="text"
+            />
+            
             <p>Position</p>
-            <input type="text" />
+            <input
+              onChange={this.handleInput}
+              value={position}
+              name="position"
+              type="text"
+            />
             <p>Date Applied</p>
-            <input type="date" />
+            <input
+              onChange={this.handleDate}
+              value={date}
+              name="date"
+              type="date"
+            />
             <p>Job Posting Url</p>
-            <input type="text" />
+            <input
+              onChange={this.handleInput}
+              value={url}
+              name="url"
+              type="text"
+            />
+            <input type="submit" value="Next" />
           </form>
         </div>
-        <div className="add-job-resume">
+        <div style={{ display: 'none' }} className="add-job-resume">
           <h3>Resume</h3>
           <form>
             <input type="file" />
           </form>
         </div>
-        <div className="add-job-coverletter">
+        <div style={{ display: 'none' }} className="add-job-coverletter">
           <h3>Cover Letter</h3>
           <form>
             <input type="file" />
           </form>
         </div>
-        <div className="add-job-interview">
+        <div style={{ display: 'none' }} className="add-job-interview">
           <h3>Interview 1</h3>
+          <form>
+            <p>Date</p>
+            <input type="date" />
+            <p>Time</p>
+            <input type="time" />
+            <p>Contact</p>
+            <input type="text" />
+          </form>
+        </div>
+        <div style={{ display: 'none' }} className="add-job-interview">
+          <h3>Interview 2</h3>
+          <form>
+            <p>Date</p>
+            <input type="date" />
+            <p>Time</p>
+            <input type="time" />
+            <p>Contact</p>
+            <input type="text" />
+          </form>
+        </div>
+        <div style={{ display: 'none' }} className="add-job-interview">
+          <h3>Interview 3</h3>
           <form>
             <p>Date</p>
             <input type="date" />
