f300c5b78a75252fffc0ecf1eea800ef3682ff5a
ramborell23 <jerelldavis@ac.c4q.nyc>
2018-03-19 11:04:17 -0400

    Fixed password digest column and Phone number

--- backend/db/elevate.sql
+++ backend/db/elevate.sql
@@ -7,11 +7,11 @@ CREATE TABLE Users
     id SERIAL UNIQUE,
     first_name VARCHAR(13),
     last_name VARCHAR(13),
-    password_digest VARCHAR(13),
-    email VARCHAR(27) UNIQUE,
-    phone_number INTEGER (9) UNIQUE,
-    email_notification VARCHAR(20),
-    phone_notification VARCHAR(20),
+    password_digest VARCHAR NOT NULL,
+    email VARCHAR(57) UNIQUE,
+    phone_number VARCHAR (10) UNIQUE,
+    email_notification VARCHAR(1),
+    phone_notification VARCHAR(1),
     experience INTEGER,
     PRIMARY Key (id)
 );
@@ -20,20 +20,12 @@ CREATE TABLE Users
 INSERT INTO Users
     (first_name, last_name, password_digest, email, phone_number,email_notification,phone_notification, experience)
 VALUES
-    ('Jerell', 'Davis', 'home', 'blah@gmail.com', '341-233-1093', 'N', 'N', 0),
-    ('Nick', 'Davis', 'home', 'blah@ghmail.com', '302-233-1093', 'N', 'N', 0),
-    ('Bob', 'Davis', 'home', 'blahhh@gmail.com', '942-233-1093', 'N', 'N', 0);
+    ('Jerell', 'Davis', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blah@gmail.com', '3412331093', 'N', 'N', 0),
+    ('Nick', 'Davis', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blah@ghmail.com', '3022331093', 'N', 'N', 0),
+    ('Bob', 'Davis', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blahhh@gmail.com', '9422331093', 'N', 'N', 0);
 
 
-Drop TABLE Users_Personal_Info CASCADE;
-CREATE TABLE Users_Personal_Info
-(
-    id INTEGER NOT NULL,
-    resume_url VARCHAR NOT NULL,
-    cover_letter_url VARCHAR(23),
-    cover_letter_2_url VARCHAR(23),
-    FOREIGN KEY (id) REFERENCES Users(id)
-);
+
 
 Drop TABLE Jobs CASCADE;
 CREATE TABLE Jobs
@@ -42,12 +34,12 @@ CREATE TABLE Jobs
     user_id INTEGER NOT NULL,
     company_name VARCHAR(27),
     position_title VARCHAR(16),
-    job_email VARCHAR(23),
-    job_phone_number VARCHAR(16),
-    progress_in_search VARCHAR(3),
+    job_email VARCHAR(53),
+    job_phone_number VARCHAR(10),
+    progress_in_search VARCHAR(1),
     salary VARCHAR(20),
     date_applied VARCHAR(20),
-    notes VARCHAR(50),
+    notes VARCHAR,
     PRIMARY KEY (job_id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
@@ -55,12 +47,12 @@ CREATE TABLE Jobs
 INSERT INTO Jobs
     (user_id, company_name, position_title, job_email,job_phone_number, progress_in_search,salary, date_applied,notes)
 VALUES
-    (1,'Apple','Junior Developer','blurb@gmail','347-121-8976','A','70,000','2-12-2018','N/A'),
-    (3,'Apple','Junior Developer','threeblurb@gmail','347-121-8976','A','70,000','2-10-2018','N/A'),
-    (1,'Apple','Junior Developer','blurb@gmail','347-121-8976','A','70,000','2-03-2018','N/A'),
-    (2,'Apple','Junior Developer','blurnnb@gmail','347-121-8976','A','70,000','2-02-2018','N/A'),
-    (1,'Apple','Junior Developer','blurnnb@gmail','347-121-8976','A','70,000','2-22-2018','N/A'),
-    (3,'Apple','Junior Developer','blurnnb@gmail','347-121-8976','A','70,000','2-17-2018','N/A');
+    (1,'Apple','Junior Developer','blurb@gmail','3471218976','A','70,000','2122018','N/A'),
+    (3,'Apple','Junior Developer','threeblurb@gmail','3471218976','A','70,000','2102018','N/A'),
+    (1,'Apple','Junior Developer','blurb@gmail','3471218976','A','70,000','2032018','N/A'),
+    (2,'Apple','Junior Developer','blurnnb@gmail','3471218976','A','70,000','2022018','N/A'),
+    (1,'Apple','Junior Developer','blurnnb@gmail','3471218976','A','70,000','2222018','N/A'),
+    (3,'Apple','Junior Developer','blurnnb@gmail','3471218976','A','70,000','2172018','N/A');
   
 
 
@@ -71,7 +63,7 @@ CREATE TABLE Resumes
     resume_id SERIAL UNIQUE,
     user_id INTEGER NOT NULL,
     job_id INTEGER NOT NULL,
-    resume_url VARCHAR(20),
+    resume_url VARCHAR,
     FOREIGN KEY (job_id) REFERENCES Jobs(job_id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
@@ -79,9 +71,44 @@ CREATE TABLE Resumes
 INSERT INTO Resumes
     (user_id,job_id, resume_url)
 VALUES
-    (1, 1, '@blurbs.com'),
-    (2, 2, '@blurb.com'),
-    (3, 3, '@blurssbssss.com'),
-    (1, 4, '@blurbs.com'),
-    (2, 5, '@blurb.com');
+    (1, 1, 'davidsh.in/resume.pdf'),
+    (1, 2, 'davidsh.in/resume.pdf'),
+    (1, 3, 'http://helencho.io/images/helenchoresume.pdf'),
+    (1, 4, 'file:///Users/c4q/Downloads/Jerell-Davis%20(2).pdf'),
+    (1, 5, '@blurb.com');
 
+
+
+
+Drop TABLE Cover_Letter;
+Drop TABLE Cover_Letters CASCADE;
+CREATE TABLE Cover_Letters
+(
+    cover_letter_id SERIAL UNIQUE,
+    user_id INTEGER NOT NULL,
+    job_id INTEGER NOT NULL,
+    cover_letter_url VARCHAR,
+    FOREIGN KEY (job_id) REFERENCES Jobs(job_id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+Drop TABLE Rank_Badges ;
+CREATE TABLE Rank_Badges
+(
+    badge_id VARCHAR,
+    badge_url VARCHAR,
+    badge_name VARCHAR,
+    PRIMARY KEY (badge_id)
+
+
+);
+
+Drop TABLE Acheivement_Badges ;
+Drop TABLE Achievement_Badges ;
+CREATE TABLE Achievement_Badges
+(
+    badge_id VARCHAR,
+    badge_url VARCHAR,
+    badge_name VARCHAR,
+    PRIMARY KEY (badge_id)
+)
