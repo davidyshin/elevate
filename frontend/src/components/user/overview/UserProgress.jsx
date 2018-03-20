5eb9b3d5c369938f3920eeae21135fddc149fd79
helencho <helencho@ac.c4q.nyc>
2018-03-20 12:06:50 -0400

    finish basic styling for user profile's progress card

--- frontend/src/components/user/overview/UserProgress.jsx
+++ frontend/src/components/user/overview/UserProgress.jsx
@@ -1,5 +1,3 @@
-// Add Job Form
-
 import React, { Component } from 'react';
 
 class UserProgress extends Component {
@@ -11,15 +9,20 @@ class UserProgress extends Component {
   render() {
     return (
       <div className="user-progress-container">
+
         <div className="user-progress-header">
           <h3>Level Progress</h3>
         </div>
+
         <div className="user-progress-content">
+
           <div className="user-progress-left">
+
             <div className="user-progress-badge-container">
               <img src="https://lh3.googleusercontent.com/1GmLSLTSH4LmI-xD5ZAYIG3DkJ4GVhAF15UbwzuPm2UgM0MvHR05_attKfkyOzJmS6kNfEXqO0wWzIzRP-FJ=w1438-h780" alt="badge" class="user-progress-badge" />
               <img src="https://lh3.googleusercontent.com/1GmLSLTSH4LmI-xD5ZAYIG3DkJ4GVhAF15UbwzuPm2UgM0MvHR05_attKfkyOzJmS6kNfEXqO0wWzIzRP-FJ=w1438-h780" alt="badge" class="user-progress-badge badge-inactive" />
             </div>
+
             <div className="user-progress-bar-container">
               <div className="user-progress-bar-total">
                 <div className="user-progress-bar-earned" />
@@ -27,13 +30,22 @@ class UserProgress extends Component {
               </div>
             </div>
           </div>
+
           <div className="user-progress-right">
-            <h3>Current Level</h3>
-            <h4>Novice</h4>
-            <h3>Next Level</h3>
-            <h4>Amateur</h4>
-            <p>400 points more to next level!</p>
+            <div className="user-progress-blank" />
+            <div className="user-progress-current">
+              <h3>Current Level</h3>
+              <h4>Novice</h4>
+            </div>
+            <div className="user-progress-next">
+              <h3>Next Level</h3>
+              <h4>Amateur</h4>
+            </div>
+            <div className="user-progress-message">
+              <p>400 points more to next level!</p>
+            </div>
           </div>
+
         </div>
       </div>
     )
