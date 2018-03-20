50eabf72bd57e12e7a76efd09588ef202d199ac4
helencho <helencho@ac.c4q.nyc>
2018-03-20 10:58:14 -0400

    implement badge hover effect

--- frontend/src/components/user/overview/UserProgress.jsx
+++ frontend/src/components/user/overview/UserProgress.jsx
@@ -18,20 +18,21 @@ class UserProgress extends Component {
           <div className="user-progress-left">
             <div className="user-progress-badge-container">
               <img src="https://lh3.googleusercontent.com/1GmLSLTSH4LmI-xD5ZAYIG3DkJ4GVhAF15UbwzuPm2UgM0MvHR05_attKfkyOzJmS6kNfEXqO0wWzIzRP-FJ=w1438-h780" alt="badge" class="user-progress-badge" />
-              <img src="https://lh3.googleusercontent.com/1GmLSLTSH4LmI-xD5ZAYIG3DkJ4GVhAF15UbwzuPm2UgM0MvHR05_attKfkyOzJmS6kNfEXqO0wWzIzRP-FJ=w1438-h780" alt="badge" class="user-progress-badge" />
+              <img src="https://lh3.googleusercontent.com/1GmLSLTSH4LmI-xD5ZAYIG3DkJ4GVhAF15UbwzuPm2UgM0MvHR05_attKfkyOzJmS6kNfEXqO0wWzIzRP-FJ=w1438-h780" alt="badge" class="user-progress-badge badge-inactive" />
             </div>
             <div className="user-progress-bar-container">
               <div className="user-progress-bar-total">
                 <div className="user-progress-bar-earned" />
+                <p>1600/2000</p>
               </div>
             </div>
           </div>
           <div className="user-progress-right">
             <h3>Current Level</h3>
-            <p>Novice</p>
+            <h4>Novice</h4>
             <h3>Next Level</h3>
-            <p>Amateur</p>
-            <p>XXX more to next level!</p>
+            <h4>Amateur</h4>
+            <p>400 points more to next level!</p>
           </div>
         </div>
       </div>
