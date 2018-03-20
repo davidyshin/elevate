8c218e6502dd274e631a0c906dae1b035ec579a3
helencho <helencho@ac.c4q.nyc>
2018-03-19 22:56:38 -0400

    setup the structure for user profile and add class names

--- frontend/src/components/user/overview/UserOverview.jsx
+++ frontend/src/components/user/overview/UserOverview.jsx
@@ -1,5 +1,3 @@
-// Add Job Form
-
 import React, { Component } from 'react';
 import UserActivity from './UserActivity.jsx';
 import UserProgress from './UserProgress.jsx';
@@ -13,17 +11,19 @@ class UserOverview extends Component {
   renderUserActivity = () => {
     return <UserActivity activeUser={this.props.activeUser} />;
   };
+
   renderUserProgress = () => {
     return <UserProgress activeUser={this.props.activeUser} />;
   };
+
   renderUserStats = () => {
     return <UserStats activeUser={this.props.activeUser} />;
   };
 
   render() {
     return (
-      <div ClassName="user-profile">
-        <h1> {this.props.activeUser.username} User Overview</h1>
+      <div ClassName="user-overview-container">
+        <h3>User Overview</h3>
         <this.renderUserProgress />
         <this.renderUserActivity />
         <this.renderUserStats />
