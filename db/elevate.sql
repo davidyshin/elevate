87bd408d235175098626e1f582a5c4d0c1c313ff
ramborell23 <jerelldavis@ac.c4q.nyc>
2018-03-20 17:22:03 -0400

    Update to queries now containing proper error messages

--- db/elevate.sql
+++ db/elevate.sql
@@ -3,7 +3,8 @@ CREATE DATABASE elevate;
 
 \c elevate;
 
-Drop TABLE Users CASCADE;
+Drop TABLE Users
+CASCADE;
 CREATE TABLE Users
 (
     id SERIAL UNIQUE,
@@ -22,21 +23,22 @@ CREATE TABLE Users
 INSERT INTO Users
     (first_name, last_name, photo_url, password_digest, username, phone_number, email_notification, phone_notification, experience)
 VALUES
-    ('Jerell', 'Davis','https://avatars3.githubusercontent.com/u/12574319?s=400&v=4', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blah@gmail.com', '3412331093', 'N', 'N', 0),
-    ('Nick', 'Davis','https://avatars3.githubusercontent.com/u/12574319?s=400&v=4', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blah@ghmail.com', '3022331093', 'N', 'N', 0),
-    ('Bob', 'Davis','https://avatars3.githubusercontent.com/u/12574319?s=400&v=4', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blahhh@gmail.com', '9422331093', 'N', 'N', 0);
+    ('Jerell', 'Davis', 'https://avatars3.githubusercontent.com/u/12574319?s=400&v=4', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blah@gmail.com', '3412331093', 'N', 'N', 0),
+    ('Nick', 'Davis', 'https://avatars3.githubusercontent.com/u/12574319?s=400&v=4', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blah@ghmail.com', '3022331093', 'N', 'N', 0),
+    ('Bob', 'Davis', 'https://avatars3.githubusercontent.com/u/12574319?s=400&v=4', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'blahhh@gmail.com', '9422331093', 'N', 'N', 0);
 
 
 
 
-Drop TABLE Jobs CASCADE;
+Drop TABLE Jobs
+CASCADE;
 CREATE TABLE Jobs
 (
     job_id SERIAL UNIQUE,
     user_id INTEGER NOT NULL,
     resume_url VARCHAR ,
     cover_url VARCHAR ,
-    company_name VARCHAR(27),
+    job_posting_url VARCHAR(27),
     company_logo VARCHAR(57),
     company_url VARCHAR(57),
     position_title VARCHAR(16),
@@ -44,76 +46,32 @@ CREATE TABLE Jobs
     job_phone_number VARCHAR(10),
     progress_in_search VARCHAR(1),
     salary VARCHAR(20),
-    date_applied DATE ,
+    date_applied DATE,
     PRIMARY KEY (job_id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 INSERT INTO Jobs
-    (user_id, company_name,resume_url, cover_url, company_logo, company_url,  position_title, job_email, job_phone_number, progress_in_search, salary)
+    (user_id, company_name, resume_url, cover_url, company_logo, job_posting_url, position_title, job_email, job_phone_number, progress_in_search, salary)
 VALUES
-    (1, 'Apple', 'RESUME URL','COVER URL','COMPANY LOGO','company_url', 'Junior Developer', 'blurb@gmail', '3471218976', 'A', '70,000'),
-    (2, 'Apple', 'RESUME URL','COVER URL','COMPANY LOGO','company_url' ,'Junior Developer', 'blurnnb@gmail', '3471218976', 'A','70,000'),
-    (1, 'Apple', 'RESUME URL','COVER URL','COMPANY LOGO','company_url' ,'Junior Developer', 'blurb@gmail', '3471218976', 'A','70,000'),
-    (2, 'Apple', 'RESUME URL','COVER URL','COMPANY LOGO','company_url','Junior Developer', 'blurnnb@gmail', '3471218976', 'A','70,000'),
-    (3, 'Apple', 'RESUME URL','COVER URL','COMPANY LOGO','company_url', 'Junior Developer', 'threeblurb@gmail', '3471218976', 'A', '70,000'),
-    (3, 'Apple', 'RESUME URL','COVER URL','COMPANY LOGO','company_url','Junior Developer', 'blurnnb@gmail', '3471218976', 'A','70,000');
-  
+    (1, 'Apple', 'RESUME URL', 'COVER URL', 'COMPANY LOGO', 'company_url', 'Junior Developer', 'blurb@gmail', '3471218976', 'A', '70,000'),
+    (2, 'Apple', 'RESUME URL', 'COVER URL', 'COMPANY LOGO', 'company_url' , 'Junior Developer', 'blurnnb@gmail', '3471218976', 'A', '70,000'),
+    (1, 'Apple', 'RESUME URL', 'COVER URL', 'COMPANY LOGO', 'company_url' , 'Junior Developer', 'blurb@gmail', '3471218976', 'A', '70,000'),
+    (2, 'Apple', 'RESUME URL', 'COVER URL', 'COMPANY LOGO', 'company_url', 'Junior Developer', 'blurnnb@gmail', '3471218976', 'A', '70,000'),
+    (3, 'Apple', 'RESUME URL', 'COVER URL', 'COMPANY LOGO', 'company_url', 'Junior Developer', 'threeblurb@gmail', '3471218976', 'A', '70,000'),
+    (3, 'Apple', 'RESUME URL', 'COVER URL', 'COMPANY LOGO', 'company_url', 'Junior Developer', 'blurnnb@gmail', '3471218976', 'A', '70,000');
+
 Drop TABLE Resumes CASCADE;
-CREATE TABLE Resumes
-(
-    resume_id SERIAL UNIQUE,
-    user_id INTEGER NOT NULL,
-    job_id INTEGER NOT NULL,
-    resume_url VARCHAR,
-    FOREIGN KEY (job_id) REFERENCES Jobs(job_id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
-);
-
-INSERT INTO Resumes
-    (user_id, job_id, resume_url)
-VALUES
-    (1, 2, 'davidsh.in/resume.pdf'),
-    (1, 2, 'davidsh.in/resume.pdf'),
-    (1, 3, 'http://helencho.io/images/helenchoresume.pdf'),
-    (2, 4, 'file:///Users/c4q/Downloads/Jerell-Davis%20(2).pdf'),
-    (3, 5, '@blurb.com');
-
-
-
-
 Drop TABLE Cover_Letters CASCADE;
-CREATE TABLE Cover_Letters
-(
-    cover_letter_id SERIAL UNIQUE,
-    user_id INTEGER NOT NULL,
-    job_id INTEGER NOT NULL,
-    cover_letter_url VARCHAR,
-    FOREIGN KEY (job_id) REFERENCES Jobs(job_id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
-);
-
-INSERT INTO Cover_Letters
-    (user_id, job_id, cover_letter_url)
-VALUES
-    (1, 1, 'davidsh.in/coverletter.pdf'),
-    (1, 2, 'davidsh.in/coverletter.pdf'),
-    (1, 3, 'http://helencho.io/images/helenchocoverletter.pdf'),
-    (2, 4, 'file:///Users/c4q/Downloads/Jerell-Daviscoverletter.pdf'),
-    (3, 5, '@blurb.com');
-
-
-
--- Drop TABLE Interview CASCADE;
 CREATE TABLE Interview
 (
     job_id INTEGER,
-    contacts VARCHAR,
+    contact VARCHAR,
     note VARCHAR,
     FOREIGN KEY (job_id) REFERENCES Jobs(job_id)
-); 
+);
 INSERT INTO Interview
-    (job_id, contacts, note)
+    (job_id, contact, note)
 VALUES
     (1, 'First Note', 'davidsh.in/coverletter.pdf'),
     (2, 'Second Note', 'davidsh.in/coverletter.pdf'),
@@ -136,12 +94,13 @@ CREATE TABLE Rank_Badges
 );
 
 INSERT INTO Rank_Badges
-    (badge_url, badge_name,badge_level)
+    (badge_url, badge_name, badge_level)
 VALUES
     ('https://drive.google.com/file/d/1Ig8MivzctCcUDuz7UqSjLzgar4p8-5ue/view?usp=sharing', 'rookie', '2');
 
 
-Drop TABLE Achievement_Badges CASCADE;
+Drop TABLE Achievement_Badges
+CASCADE;
 CREATE TABLE Achievement_Badges
 (
     badge_id SERIAL UNIQUE,
@@ -153,10 +112,10 @@ CREATE TABLE Achievement_Badges
 INSERT INTO Achievement_Badges
     (badge_url, badge_name)
 VALUES
-    ('https://drive.google.com/file/d/16fcWlOZQH-bc98Tc-dSvkeQFXkyfJGqo/view?usp=sharing','25-apps'),
-    ('https://drive.google.com/file/d/1mGrNYmuCmUn4L_SDRDaqGI3xoI2Ake5B/view?usp=sharing','50-apps'),
-    ('https://drive.google.com/file/d/121mVMSFP6CxmBng1Y79L9kNsF2vnGSjo/view?usp=sharing','100-apps'),
-    ('https://drive.google.com/file/d/1a4KZJ5EUiZ5nrmGs_VF_9ygBlP-4Ce8r/view?usp=sharing','200-apps');
+    ('https://drive.google.com/file/d/16fcWlOZQH-bc98Tc-dSvkeQFXkyfJGqo/view?usp=sharing', '25-apps'),
+    ('https://drive.google.com/file/d/1mGrNYmuCmUn4L_SDRDaqGI3xoI2Ake5B/view?usp=sharing', '50-apps'),
+    ('https://drive.google.com/file/d/121mVMSFP6CxmBng1Y79L9kNsF2vnGSjo/view?usp=sharing', '100-apps'),
+    ('https://drive.google.com/file/d/1a4KZJ5EUiZ5nrmGs_VF_9ygBlP-4Ce8r/view?usp=sharing', '200-apps');
 
 
 
@@ -168,10 +127,10 @@ CREATE TABLE Achievement_Badges_Earned
     badge_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES Users(id),
     FOREIGN KEY (badge_id) REFERENCES Achievement_Badges(badge_id)
-); 
+);
 
 INSERT INTO Achievement_Badges_Earned
-    (user_id, badge_id) 
+    (user_id, badge_id)
 VALUES
     (1, 1),
     (1, 1),
--- db/queries.js
+++ db/queries.js
@@ -15,7 +15,7 @@ function registerUser(req, res, next) {
         photo_url: 'https://avatars3.githubusercontent.com/u/12574319?s=400&v=4',
         password: hash,
         phoneNumber: req.body.user.phoneNumber,
-        experience: '0'
+        experience: 0
       }
     )
     .then(() => {
@@ -24,7 +24,7 @@ function registerUser(req, res, next) {
       });
     })
     .catch(err => {
-      console.log(`this is your error`, err);
+      console.log(`Registration`, err);
       res.status(500).json({
         message: `Registration Failed   ${err} `,
         err
@@ -37,57 +37,58 @@ function logoutUser(req, res, next) {
   res.status(200).send('log out success');
 }
 
-// post
-
 function getUser(req, res, next) {
   // console.log(req)
   db
-    .one("SELECT * FROM Users WHERE id=${id}", {
+    .one('SELECT * FROM Users WHERE id=${id}', {
       id: req.user.id
     })
     .then(data => {
       res.status(200).json({ user: data });
+    })
+    .catch((err) => {
+      res.status(500).send('error getting user');
+      return next(err)
     });
 }
-
 
 // For route = /users/getResume/:id/:job
 
 function getResume(req, res, next) {
   let id = req.params.id
-  let job= req.params.job
+  let job = req.params.job
 
   db
-    .any('SELECT *  FROM resumes WHERE user_id = $1 and job_id = $2',[id, job])
+    .any('SELECT resume_url  FROM jobs WHERE user_id = $1 and job_id = $2', [id, job])
     .then((data) => {
       res.status(200).json({
         status: 'success',
-        data: data,
+        resume_url: data,
         message: 'Retrieved user resume'
       })
     })
     .catch((err) => {
-      return next(err)
+      res.status(500).send('error getting job resume');
     })
 }
 
 // For route = /users/getCoverLetter/:id/:job
 function getCoverLetter(req, res, next) {
   let id = req.params.id
-  let job= req.params.job
+  let job = req.params.job
   db
-      .any('SELECT *  FROM cover_letters WHERE user_id=$1 and job_id = $2',[id, job])
-      .then((data) => {
-          res.status(200).json({
-              status: 'success',
-              data: data,
-              message: 'Retrieved user cover letters'
-          })
+    .any('SELECT cover_url  FROM jobs WHERE user_id=$1 and job_id = $2', [id, job])
+    .then((data) => {
+      res.status(200).json({
+        status: 'success',
+        cover_url: data,
+        message: 'Retrieved job cover letters'
       })
-      .catch((err) => {
-          return next(err)
-      })
-} 
+    })
+    .catch((err) => {
+      res.status(500).send('error getting job cover letter');
+    })
+}
 
 
 
@@ -96,141 +97,107 @@ function getCoverLetter(req, res, next) {
 function getAllUserApps(req, res, next) {
   let id = req.params.id
   db
-      .any('SELECT *  FROM jobs WHERE user_id=$1',[id])
-      .then((data) => {
-          res.status(200).json({
-              status: 'success',
-              data: data,
-              message: 'Retrieved user cover letters'
-          })
+    .any('SELECT * FROM jobs WHERE user_id=$1', [id])
+    .then((data) => {
+      res.status(200).json({
+        status: 'success',
+        applications: data,
+        message: 'Retrieved all job apps for user'
       })
-      .catch((err) => {
-          return next(err)
-      })
-} 
+    })
+    .catch((err) => {
+      res.status(500).send('error getting all job applications for user');
+      return next(err)
+    })
+}
 
 function getUserAchievementBadges(req, res, next) {
   let id = req.user.id
   db
-      .any('SELECT *  FROM achievement_badges JOIN achievement_badges_earned ON  achievement_badges_earned.badge_id  = achievement_badges.badge_id  WHERE user_id=$1',[id])
-      .then((data) => {
-          res.status(200).json({
-              status: 'success',
-              data: data,
-              message: 'Retrieved User Achievement Badges'
-          })
+    .any('SELECT * FROM achievement_badges JOIN achievement_badges_earned ON achievement_badges_earned.badge_id = achievement_badges.badge_id WHERE user_id=$1', [id])
+    .then((data) => {
+      res.status(200).json({
+        status: 'success',
+        data: data,
+        message: 'Retrieved User Achievement Badges earned'
       })
-      .catch((err) => {
-          return next(err)
-      })
-} 
-
-function updateResume(req, res, next) {
-  db.none(
-      "INSERT INTO resumes ( user_id, job_id, resume_url) VALUES (${user_id}, ${job_id}, ${resume_url})"
-      ,{ 
-        resume_url: req.body.resume_url,  
-        job_id: req.body.job_id,
-        user_id: req.user.id
-      }
-    )
-    .then(() => {
-      res.send(`created user: ${req.body.username}`);
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).send("error creating user");
-    });
+    .catch((err) => {
+      res.status(500).send('error getting user achievement badges earned');
+    })
 }
 
 function updateCoverLetter(req, res, next) {
   db.none(
-      "UPDATE jobs SET cover_url = ${cover_url} WHERE job_id = ${job_id}"
-      ,{ 
-        cover_url: req.body.cover_url,  
-        job_id: req.body.job_id,
-      }
-    )
+    'UPDATE jobs SET cover_url = ${cover_url} WHERE job_id = ${job_id}'
+    , {
+      cover_url: req.body.cover_url,
+      job_id: req.body.job_id,
+    }
+  )
     .then(() => {
-      res.send(`Updated Cover Letter for user: ${req.body.job_id}`);
+      res.send(`Updated Cover Letter for user`);
     })
     .catch(err => {
       console.log(err);
-      res.status(500).send("error creating user");
+      res.status(500).send('error updating cover letter');
     });
 }
 
 function updateResume(req, res, next) {
   db.none(
-      "UPDATE jobs SET resume_url = ${resume_url} WHERE job_id = ${job_id}"
-      ,{ 
-        resume_url: req.body.resume_url,  
-        job_id: req.body.job_id,
-      }
-    )
+    'UPDATE jobs SET resume_url = ${resume_url} WHERE job_id = ${job_id}'
+    , {
+      resume_url: req.body.resume_url,
+      job_id: req.body.job_id,
+    }
+  )
     .then(() => {
-      res.send(`Updated Cover Letter for user: ${req.body.job_id}`);
+      res.send(`Updated Resume for user`);
     })
     .catch(err => {
       console.log(err);
-      res.status(500).send("error creating user");
+      res.status(500).send('error updating resume');
     });
 }
 
-
-
 function createJobApp(req, res, next) {
   db.none(
-      "INSERT INTO jobs ( user_id, company_name, company_logo, date_applied, position_title, job_email, job_phone_number) VALUES (${user_id}, ${job_id}, ${cover_letter_url})"
-      ,{ 
-        user_id: req.user.id,
-        company_name: req.body.company_name,  
-        company_logo: req.body.company_logo,  
-        date_applied: req.body.date_applied,  
-        job_email: req.body.job_email,  
-        job_phone_number: req.body.job_phone_number,  
-        position_title: req.body.position_title,  
-        company_url: req.body.url,  
-      }
-    )
+    'INSERT INTO jobs ( user_id, company_name, company_logo, date_applied, position_title, job_email, job_phone_number) VALUES (${user_id}, ${job_id}, ${cover_letter_url})'
+    , {
+      user_id: req.user.id,
+      company_name: req.body.company_name,
+      company_logo: req.body.company_logo,
+      date_applied: req.body.date_applied,
+      job_email: req.body.job_email,
+      job_phone_number: req.body.job_phone_number,
+      position_title: req.body.position_title,
+      company_url: req.body.url,
+    }
+  )
     .then(() => {
-      res.send(`Created Job App for : ${req.user.username}`);
+      res.send(`Created Job App`);
     })
     .catch(err => {
       console.log(err);
-      res.status(500).send("error creating job app ");
+      res.status(500).send('error creating job app');
     });
 }
 
 // Get rank badge earned 
 function getRankedBadge(req, res, next) {
   db.any(
-      "Select badge_url from rank_badges Where badge_level = ${level}",
-      {level: req.params.level}
-    )
+    'SELECT badge_url from rank_badges WHERE badge_level = ${level}',
+    { level: req.params.level }
+  )
     .then((data) => {
       res.send({
-        data : data
+        badge: data
       });
     })
     .catch(err => {
       console.log(err);
-      res.status(500).send("error creating job app ");
-    });
-}
-function getUserAchievementBadges(req, res, next) {
-  db.any(
-      "Select badge_url and  from rank_badges Where badge_level = ${level} ",
-      {level: req.params.level}
-    )
-    .then((data) => {
-      res.send({
-        data : data
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).send("error creating job app ");
+      res.status(500).send('error getting ranked badge');
     });
 }
 
@@ -238,75 +205,74 @@ function getUserAchievementBadges(req, res, next) {
 function updateUsersInfo(req, res, next) {
   db
     .none(
-      "update users set username = ${username}, phone_number = ${phone_number} where id = ${id}",
+      'UPDATE users SET username = ${username}, phone_number = ${phone_number} WHERE id = ${id}',
       {
         username: req.body.username,
         phone_number: req.body.phone_number,
         id: req.user.id
-
       }
     )
-    .then(function(data) {
+    .then(function (data) {
       res.status(200).json({
-        status: "success",
-        message: "Changed user information"
+        status: 'success',
+        message: 'Changed user information'
       });
     })
-    .catch(function(err) {
+    .catch(function (err) {
+      res.status(500).send('error updating user information');
       return next(err);
     });
 
-  }
+}
 
 
-function updateUserInterview(req, res, next) {
+function updateJobInterview(req, res, next) {
   db
     .none(
-      "update jobs set interview_1 = ${interview_1}, interview_1_notes = ${interview_1_notes}, interview_1_contact = ${interview_1_contact}  where job_id = ${job_id}",
+      'UPDATE Interview SET contacts = ${contact}, note = ${note} WHERE job_id = ${job_id}',
       {
-        username: req.body.username,
-        interview_1_date: req.body.date,
-        interview_1_notes: req.body.notes,
-        interview_1_contact: req.body.contact,
-        id: req.body.job_id
-
+        contact: req.body.contact,
+        note: req.body.note,
+        job_id: req.body.job_id
       }
     )
-    .then(function(data) {
+    .then(function (data) {
       res.status(200).json({
-        status: "success",
-        message: "Changed user information"
+        status: 'success',
+        message: 'updated job interview'
       });
     })
-    .catch(function(err) {
-      return next(err);
+    .catch(function (err) {
+      res.status(500).send('Error updating job interview');
     });
+}
 
-  }
+function getUserExp(req, res, next) {
+  db
+    .one('SELECT experience FROM Users WHERE id=${id}', {
+      id: req.user.id
+    })
+    .then(data => {
+      res.status(200).json({ exp: data });
+    })
+    .catch(function (err) {
+      res.status(500).send('Error getting user experience');
+    });
+}
 
+function getJobInterview(req, res, next) {
+  db
+    .one('SELECT * FROM Interview WHERE job_id=${job_id}', {
+      job_id: req.body.job_id
+    })
+    .then(data => {
+      res.status(200).json({ interview: data });
+    })
+    .catch(function (err) {
+      res.status(500).send('Error getting job interview');
+    });
+}
 
-  function getUserExp(req, res, next) {
-    // console.log(req)
-    db
-      .one("SELECT experience FROM Users WHERE id=${id}", {
-        id: req.user.id
-      })
-      .then(data => {
-        res.status(200).json({ exp: data });
-      });
-  }
-  
-  function getUserInterview(req, res, next) {
-    // console.log(req)
-    db
-      .one("SELECT * FROM Interview WHERE job_id=${job_id}", {
-        id: req.user.id
-      })
-      .then(data => {
-        res.status(200).json({ exp: data });
-      });
-  }
-  
 
 module.exports = {
   registerUser,
