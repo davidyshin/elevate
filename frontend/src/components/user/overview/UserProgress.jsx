2f1503d4f1d79cc84b8f77372b7b7c6333bca43c
helencho <helencho@ac.c4q.nyc>
2018-03-19 22:56:38 -0400

    setup the structure for user profile and add class names

--- frontend/src/components/user/overview/UserProgress.jsx
+++ frontend/src/components/user/overview/UserProgress.jsx
@@ -7,8 +7,32 @@ class UserProgress extends Component {
     super();
     this.state = {};
   }
+
   render() {
-      return (<h1>{this.props.activeUser.username} User Progress</h1>)
+    return (
+      <div className="user-progress-container">
+        {/* <h3>Progress</h3> */}
+        <div className="user-progress-header">
+          <h3>User Progress</h3>
+        </div>
+        <div className="user-progress-content">
+          <div className="user-progress-left">
+            <div className="user-progress-badge-container">
+              <img src="https://lh3.googleusercontent.com/1GmLSLTSH4LmI-xD5ZAYIG3DkJ4GVhAF15UbwzuPm2UgM0MvHR05_attKfkyOzJmS6kNfEXqO0wWzIzRP-FJ=w1438-h780" alt="badge" class="user-progress-badge" />
+              <img src="https://lh3.googleusercontent.com/1GmLSLTSH4LmI-xD5ZAYIG3DkJ4GVhAF15UbwzuPm2UgM0MvHR05_attKfkyOzJmS6kNfEXqO0wWzIzRP-FJ=w1438-h780" alt="badge" class="user-progress-badge" />
+            </div>
+            <div className="user-progress-bar-container">
+            </div>
+          </div>
+          <div className="user-progress-right">
+            <h3>Current Level</h3>
+            <h3>Next Level</h3>
+            <p>XXX more to next level!</p>
+          </div>
+
+        </div>
+      </div>
+    )
   }
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import './stylesheets/index.css';
 
 ReactDOM.render(
     <BrowserRouter>
