3473a6e15a1738a8b4d94427418d00657306cd79
davidyshin <davidshin@ac.c4q.nyc>
2018-03-21 11:31:46 -0400

    Routes created, refactored queries so that userid is taken from req.user.id instead of the parameters of URL. organized routes accordingly with the queries that are already in place. references at the top of routes and queries page are identical

--- routes/users.js
+++ routes/users.js
@@ -4,39 +4,91 @@ const router = express.Router();
 const { loginRequired } = require('../auth/helpers');
 const passport = require('../auth/local');
 
-/* GET users listing. */
+/* List of queries/routes for reference
+---------------------------------------
+  GET Requests
+---------------------------------------
+ 1. getAllUserApps  // GET Route = /users/getAllUserApps
+ 2. getCoverLetter  // GET Route = /users/getCoverLetter/:job
+ 3. getJobInterview // GET Route = /users/getJobInterview/:job
+ 4. getRankedBadge  // GET Route = /users/getRankedBadge/:level
+ 5. getResume // GET Route = /users/getResume/:job
+ 6. getUser // GET Route = /users/getUser
+ 7. getUserAchievementBadges  // GET Route = /users/getUserAchieves
+ 8. getUserExp // GET Route = /users/getUserExp
+ 9. logoutUser // GET Route = /users/logout
 
-//User login/logout and registration ROUTES
+---------------------------------------
+  POST Requests
+---------------------------------------
+ 10. createJobApp // POST Route /users/createJobApp
+ 11. registerUser // POST Route = /users/newuser
+
+ ---------------------------------------
+  PUT Requests
+---------------------------------------
+ 12. updateCoverLetter // PUT Route = /users/updateCoverLetter
+ 13. updateResume // PUT Route = /users/updateResume
+ 14. updateJobInterview // PUT Route = /users/updateInterview
+ 15. updateUsersInfo // PUT Route = /users/updateInfo
+--------------------------------------- 
+*/
+
+/* ----------------------- GET Requests. ----------------------- */
+
+/*  1. getAllUserApps  // GET Route = /users/getAllUserApps */
+router.get('/getAllUserApps/', loginRequired, db.getAllUserApps);
+
+/* 2. getCoverLetter  // GET Route = /users/getCoverLetter/:job */
+router.get('/getCoverLetter/:job', loginRequired, db.getCoverLetter);
+
+/* 3. getJobInterview // GET Route = /users/getJobInterview/:job */
+router.get('/getJobInterview/:job', loginRequired, db.getJobInterview);
+
+/* 4. getRankedBadge  // GET Route = /users/getRankedBadge/:level */
+router.get('/getRankedBadge/:level', loginRequired, db.getRankedBadge);
+
+/* 5. getResume // GET Route = /users/getResume/:job */
+router.get('/getResume/:job', loginRequired, db.getResume);
+
+/* 6. getUser // GET Route = /users/getUser */
+router.get('/getUser', loginRequired, db.getUser);
+
+/* 7. getUserAchievementBadges  // GET Route = /users/getUserAchieves */
+router.get('/getUserAchieves', loginRequired, db.getUserAchievementBadges);
+
+/* 8. getUserExp // GET Route = /users/getUserExp */
+router.get('/getUserExp', loginRequired, db.getUserExp);
+
+/* 9. logoutUser // GET Route = /users/logout */
+router.get('/logout', loginRequired, db.logoutUser);
+
+/* ----------------------- POST Requests. ----------------------- */
+
+/* 10. createJobApp // POST Route /users/createJobApp */
+router.get('/createJobApp', loginRequired, db.createJobApp);
+
+/* 11. registerUser // POST Route = /users/newuser */
+router.post('/newuser', db.registerUser);
+
+/* Login User // POST Route = /users/login */
+/* This route goes through auth instead of our written queries */
 router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json(req.user);
 });
 
-router.get('/logout', loginRequired, db.logoutUser);
-router.post('/newuser', db.registerUser);
+/* ----------------------- PUT Requests. ----------------------- */
 
-//get user when app loads from session
-router.get('/getUser', loginRequired, db.getUser);
+/* 12. updateCoverLetter // PUT Route = /users/updateCoverLetter */
+router.put('/updateCoverLetter', loginRequired, db.updateCoverLetter);
 
+/* 13. updateResume // PUT Route = /users/updateResume */
+router.put('/updateResume', loginRequired, db.updateResume);
 
+/* 14. updateJobInterview // PUT Route = /users/updateInterview */
+router.put('/updateInterview', loginRequired, db.updateResume);
 
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-// // Users and Edit Users
-// router.get('/u/all', db.getAllUsers);
-// router.get('/u/:user_id', db.getSingleUser);
-
-// // Jobs and Edit Jobs
-// router.get('/j/all', db.getAllJobApps); // get all job applications
-// router.get('/j/:user_id', db.getSingleUserJobApps); // get all job applications by a specific user
-
-// // Cover Letters
-
-// // Resumes
-// router.get('/r/all', db.getAllResumes); // get all resumes
-// router.get('/r/:user_id', db.getUsersResumes); // get all resumes by a specific user
-
-// Badges
+/* 15. updateUserInfo // PUT Route = /users/updateUserInfo */
+router.put('/updateUserInfo', loginRequired, db.updateUSerInfo);
 
 module.exports = router;
