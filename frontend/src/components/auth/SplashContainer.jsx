2aaf34f99873ce0c36f5f73fea3729ba3de30c06
helencho <helencho@ac.c4q.nyc>
2018-03-20 23:34:44 -0400

    resize feature cards and redo responsiveness based on the resizing

--- frontend/src/components/auth/SplashContainer.jsx
+++ frontend/src/components/auth/SplashContainer.jsx
@@ -20,7 +20,7 @@ class SplashContainer extends Component {
 
         <div className="splash-top">
           <h1>Elevate</h1>
-          <p>Elevate is the most rewarding job application tracker.</p>
+          <p>Elevate your job search experience.</p>
           <div className="splash-top-button-container">
             <button>Demo</button>
             <button id="signup" onClick={this.props.toggleActive}>Create an account</button>
@@ -33,6 +33,7 @@ class SplashContainer extends Component {
             <div>
               <img src="https://i.imgur.com/u7NyCxx.png" alt="feature-1" />
             </div>
+            <h3>Notifications</h3>
             <p>Never forget an interview date.</p>
           </div>
 
@@ -40,6 +41,7 @@ class SplashContainer extends Component {
             <div>
               <img src="https://i.imgur.com/WB9xBxu.png" alt="feature-2" />
             </div>
+            <h3>Rewards</h3>
             <p>Earn experience points towards your success.</p>
           </div>
 
@@ -47,6 +49,7 @@ class SplashContainer extends Component {
             <div>
               <img src="https://i.imgur.com/IDI6oFc.png" alt="feature-3" />
             </div>
+            <h3>Progress</h3>
             <p>Weekly summary on your search progress.</p>
           </div>
         </div>
