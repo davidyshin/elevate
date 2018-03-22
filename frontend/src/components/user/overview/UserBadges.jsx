9132e1123ccebdefbf2b0db7f7d6e6d8fe81eb3d
helencho <helencho@ac.c4q.nyc>
2018-03-22 14:19:00 -0400

    moved axios calls into child components instead of calling from parent component

--- frontend/src/components/user/overview/UserBadges.jsx
+++ frontend/src/components/user/overview/UserBadges.jsx
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class UserBadges extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      achievements: []
+    };
+  }
+
+  componentDidMount() {
+    this.getAchievementBadges();
+  }
+
+  getAchievementBadges = () => {
+    axios
+      .get('/users/getUserAchieves')
+      .then(data => {
+        let achievements = data.data.achieves;
+        this.setState({
+          achievements
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   render() {
+    const { achievements } = this.state;
+
+    const showBadges = achievements.map(achievement => (
+      <img src={achievement.badge_url} alt={achievement.badge_url} className="user-progress-badge" />
+    ))
+
     return (
-        <div className="user-badges-container" id="badges">
-            <div className="user-badge-image-container">
-                <img src="" alt="badge" class="user-progress-badge" />
-                <img src="" alt="badge" class="user-progress-badge" />
-                <img src="" alt="badge" class="user-progress-badge" />
-            </div>
-            <h3>More Badges</h3>
+      <div className="user-badges-container" id="badges">
+        <div className="user-badge-image-container">
+          {showBadges}
         </div>
+        <h3>More Badges</h3>
+      </div>
     )
   }
 }
