cd32f67eb6bcd28c5fac056973ec05f32d8e11a5
davidyshin <davidshin@ac.c4q.nyc>
2018-03-22 18:49:40 -0400

    heroku master

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.use('/', index);
 app.use('/users', users);
