afab6844dcf3e4f6b21ff83f6a019d820887f59a
davidyshin <davidshin@ac.c4q.nyc>
2018-03-22 18:30:42 -0400

    wip

--- frontend/src/components/jobs/JobsContainer.jsx
+++ frontend/src/components/jobs/JobsContainer.jsx
@@ -10,14 +10,29 @@ import '../../stylesheets/jobs-main.css';
 class JobsContainer extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      editingJob: ''
+    };
   }
+  editJob = job => {
+    this.setState({
+      editingJob: job
+    })
+  }
+  
 
   render() {
+    const {editingJob} = this.state
     return (
       <div className="jobs-container">
-        <JobSummary activeUser={this.props.activeUser} />
-        <JobList activeUser={this.props.activeUser} />
+        {!editingJob ? (
+          <div className="active-jobs-container">
+            <JobSummary activeUser={this.props.activeUser} />
+            <JobList editJob={this.editJob} activeUser={this.props.activeUser} />
+          </div>
+        ) : (
+          <UpdateJobForm editingJob={editingJob} />
+        )}
       </div>
     );
   }
