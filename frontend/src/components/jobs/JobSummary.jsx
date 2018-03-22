30a9a2dba41ce2fcea906b6172ad595686af6576
davidyshin <davidshin@ac.c4q.nyc>
2018-03-22 11:04:19 -0400

    testing verison of plotly js , figuring out the logic to correctly render visuals

--- frontend/src/components/jobs/JobSummary.jsx
+++ frontend/src/components/jobs/JobSummary.jsx
@@ -1,17 +1,52 @@
 // ANALYTICs
 
 import React, { Component } from 'react';
+import Plot from 'react-plotly.js';
+
+// DATE FUNCTIONS
+var curr = new Date(); // get current date
+var first = curr.getDate();
+var firstday = new Date(curr.setDate(first)).toString();
+let week = [];
+for (var i = 0; i < 8; i++) {
+  var next = new Date(curr.getTime());
+  next.setDate(first - i);
+  week.push(next.toDateString());
+}
+console.log(week.reverse());
 
 class JobSummary extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      test: ""
+    };
   }
   render() {
     return (
       <div className="job-summary">
         {' '}
-        <h1>{this.props.activeUser.first_name}'s' Job Summary </h1>{' '}
+        <Plot
+          data={[
+            {
+              x: week,
+              y: [0, 0, 0, 0, 0, 0, 0, 0],
+              type: 'line',
+              marker: {
+                color: 'rgba(58,200,225,.5)',
+                line: {
+                  color: 'rbg(8,48,107)',
+                  width: 1
+                }
+              }
+            }
+          ]}
+          layout={{
+            width: 800,
+            height: 240,
+            title: `${this.props.activeUser.first_name}'s Weekly Summary`
+          }}
+        />
       </div>
     );
   }
