04cafbfd6204a6c6fbaf128c46743d88db57ef94
davidyshin <davidshin@ac.c4q.nyc>
2018-03-23 09:00:52 -0400

    implemented feature to update stage 1 and all interviews of job application

---
+++ frontend/src/components/jobs/forms/UpdateInterview.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class UpdateInterview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      date: '',
+      contact: '',
+      note: '',
+      time: ''
+    };
+  }
+
+  componentDidMount() {
+    const { interview } = this.props;
+    const date = new Date(interview.interview_date);
+    const interview_date = date.toISOString().substring(0, 10);
+    this.setState({
+      date: interview_date,
+      contact: interview.contact,
+      note: interview.note,
+      time: interview.interview_time
+    });
+  }
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { date, contact, note, time } = this.state;
+    let job_id = this.props.interview.job_id;
+    axios
+      .put('/users/UpdateInterview', {
+        contact: contact,
+        note: note,
+        interview_date: date,
+        interview_time: time,
+        job_id: job_id
+      })
+      .then(() => {
+        this.setState({
+          interviewSaved: true
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    const { date, contact, note, time } = this.state;
+    return (
+      <div className="update-interview-form">
+        <h3>Update Interview</h3>
+        <form onSubmit={this.handleSubmit}>
+          <p>Date</p>
+          <input
+            value={date}
+            onChange={this.handleInput}
+            name="date"
+            type="date"
+          />
+          <input
+            value={time}
+            onChange={this.handleInput}
+            name="time"
+            type="time"
+          />
+          <p>Contact</p>
+          <input
+            value={contact}
+            onChange={this.handleInput}
+            name="contact"
+            type="text"
+          />
+          <p>Note</p>
+          <textarea value={note} onChange={this.handleInput} name="note" />
+          <input
+            disabled={!time || !date || !contact}
+            type="submit"
+            value="Save"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UpdateInterview;
