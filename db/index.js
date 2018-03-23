e1a10649d054f4427c0bd7eacd7a47888ee7e100
davidyshin <davidshin@ac.c4q.nyc>
2018-03-22 21:51:57 -0400

    changed backend files package.json to run depployment script, changed connection string to process.env.DATABASE_URL so that our apps runs locally and on a server

--- db/index.js
+++ db/index.js
@@ -1,5 +1,11 @@
-var pgp = require("pg-promise")({});
+const pgp = require("pg-promise")({});
+const dotenv = require('dotenv')
+
+dotenv.load();
 var connectionString = process.env.DATABASE_URL
+
+
+
 var db = pgp(connectionString);
 
 module.exports = db;
