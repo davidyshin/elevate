d25c4ba3f0463e0592b482b45f9569e81bfdd3c8
helencho <helencho@ac.c4q.nyc>
2018-03-25 10:18:46 -0400

    alternate job item backgrounds depending on the index number

--- frontend/src/components/jobs/JobItem.jsx
+++ frontend/src/components/jobs/JobItem.jsx
@@ -12,8 +12,10 @@ class JobItem extends Component {
     const job = this.props.job;
     const date = new Date(this.props.job.date_applied);
     const renderDate = date.toDateString();
+    const alternateBg = (this.props.index + 1) % 2 === 1 ? 'job-item-light' : 'job-item-dark';
+
     return (
-      <div className="job-item">
+      <div className={`job-item ${alternateBg}`}>
         <p className="job-item-index">{this.props.index + 1}</p>
 
         <p className="job-item-company" onClick={this.props.handleClick} id={job.job_id}>{job.company_name}</p>
@@ -26,9 +28,6 @@ class JobItem extends Component {
         <p className="job-item-position">{job.position_title}</p>
         <p className="job-item-date">{renderDate}</p>
 
-        {/* <h3 onClick={this.props.handleClick} id={job.job_id}>
-          Info
-        </h3> */}
       </div>
     );
   }
