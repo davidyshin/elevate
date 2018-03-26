4eb891de908a488a480c90bc07a924badf6aecbc
ramborell23 <jerelldavis@ac.c4q.nyc>
2018-03-26 15:07:02 -0400

    Working version of AWS upload for master

--- frontend/src/components/jobs/forms/ResumeUpload.jsx
+++ frontend/src/components/jobs/forms/ResumeUpload.jsx
@@ -10,31 +10,37 @@ class ResumeUpload extends Component {
     }
   }
 
-handleFiles = (event) => {
-  const file = event.target.files[0];
-  console.log(event.target.files[0])  
-  console.log('MY TARGET FILE',file);
-  const data = new FormData();
-  data.append('file', file.name);
-  console.log('MY TARGET FILE',data);
-  // data.append('filename', file.name);
-  // data.append('job_id', this.props.job_id);
-  // let temp = data
-  // console.log('BEFORE I SEND THE DATA', file.name)
-  // console.log('BEFORE I SEND THE DATA', data )
-  // var formData = new FormData();
-  // formData.append('username', 'Chris');
-  // console.log('BEFORE I SEND THE DATA', formData )
-  axios.post('/users/uploadResume',  file)
+  handleFiles = (event) => {
+    const file = event.target.files[0];
+    console.log(event.target.files[0])
+    console.log('MY TARGET FILE', file);
+    const data = new FormData();
+    data.append('resume', file);
+    data.append('id', this.props.job_id);
+    // data.append('name', file.name);
+    // console.log('MY TARGET FILE', file);
+    // for (var key of data) {
+    //   console.log(key)
+    // }
+    
+    // data.append('job_id', this.props.job_id);
+    // let temp = data
+    // console.log('BEFORE I SEND THE DATA', file.name)
+    // console.log('BEFORE I SEND THE DATA', data )
+    // var formData = new FormData();
+    // formData.append('username', 'Chris');
+    // console.log('BEFORE I SEND THE DATA', formData )
+    
+    axios.post('/users/uploadResume', data)
       .then(res => {
-          console.log('FRONT RES',res)
-          this.props.handleResumeInput(res)
+        console.log('FRONT RES', res)
+        this.props.handleResumeInput(res)
       })
-      
+
       .catch(err => {
-          console.log(err)
+        console.log(err)
       })
-}
+  }
 
 
   render() {
@@ -42,12 +48,12 @@ handleFiles = (event) => {
       <div className="resume-form">
         <form id="2">
           <h1>Resume</h1>
-          <input 
-          type="file" 
-          name="jobid" 
-          id="input" 
-          onChange={this.handleFiles}
-          ></input><br/>
+          <input
+            type="file"
+            name="resume"
+            id="input"
+            onChange={this.handleFiles}
+          ></input><br />
         </form>
       </div>
     );
