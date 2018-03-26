e686b846d88c401c7765386321e59df98d1a0725
ramborell23 <jerelldavis@ac.c4q.nyc>
2018-03-26 12:17:43 -0400

    Commit to fix broxen route

--- frontend/src/components/jobs/forms/ResumeUpload.jsx
+++ frontend/src/components/jobs/forms/ResumeUpload.jsx
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 
 class ResumeUpload extends Component {
   constructor() {
     super();
+    this.state = {
+      file: null,
+    }
   }
+
+handleFiles = (event) => {
+  let file = event.target.files[0];
+  // console.log('CHECK IF FILE EXISTS: ', file)
+  console.log(file.name);
+  let data = new FormData();
+  data.append('file', file);
+  data.append('filename', file.name);
+  data.append('job_id', this.props.job_id);
+  axios.post('/users/uploadResume', {
+    data: data
+  })
+      .then(res => {
+          console.log('FRONT RES',res)
+          this.props.handleResumeInput(res)
+      })
+      
+      .catch(err => {
+          console.log(err)
+      })
+}
+
+
   render() {
     return (
       <div className="resume-form">
         <form id="2">
           <h1>Resume</h1>
-          <input
-            onChange={this.props.handleResumeInput}
-            value={this.props.resume}
-            type="file"
-          />
+          <input 
+          type="file" 
+          name="jobid" 
+          id="input" 
+          onChange={this.handleFiles}
+          ></input><br/>
         </form>
       </div>
     );
