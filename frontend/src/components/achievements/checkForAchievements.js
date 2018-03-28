9f005ac44cc946f92ab37da8bfacc8871a522ba1
davidyshin <davidshin@ac.c4q.nyc>
2018-03-28 13:26:02 -0400

    added account created achievement as badge_id 1

--- frontend/src/components/achievements/checkForAchievements.js
+++ frontend/src/components/achievements/checkForAchievements.js
@@ -1,26 +1,20 @@
 import axios from 'axios';
 
+
 /*
-
-checkJobNumber is to check the number of the job apps submitted, there is
-a switch statement for each case
-
-Function will be called as soon as user clicks "Save" on the first state of the AddJobForm
-
-badge_id: 1 === 1 (First) job app logged
-badge_id: 2 === 25 job app logs 
-badge_id: 3 === 50 job app logs 
-badge_id: 4 === 100 job app logs 
-badge_id: 5 === 200 job app logs 
-badge_id: 6 === 300 job app logs 
-badge_id: 7 === 400 job app logs 
-badge_id: 8 === 500 job app logs
-badge_id: 9 === 1 (first interview) saved
-badge_id: 10 === 5 interviews saved
-badge_id: 11 === 15 interviews saved
-
+badge_id: 1 === Account Created
+badge_id: 2 === 1st job app logged
+badge_id: 3 === 25 job app logs 
+badge_id: 4 === 50 job app logs 
+badge_id: 5 === 100 job app logs 
+badge_id: 6 === 200 job app logs 
+badge_id: 7 === 300 job app logs 
+badge_id: 8 === 400 job app logs 
+badge_id: 9 === 500 job app logs
+badge_id: 10 === 1st saved
+badge_id: 11 === 5 interviews saved
+badge_id: 12 === 15 interviews saved
 */
-
 /* Takes a badge_id as an argument and makes a post request 
 adds logged in user_id and badge_id into the achievement_badges_earned table
 */
@@ -37,6 +31,14 @@ const addAchievement = badge_id => {
     });
 };
 
+
+
+/*
+checkJobNumber is to check the number of the job apps submitted, there is
+a switch statement for each case
+
+Function will be called as soon as user clicks "Save" on the first state of the AddJobForm
+*/
 const checkJobNumber = () => {
   axios
     .get('/users/getAllUserApps')
@@ -46,35 +48,35 @@ const checkJobNumber = () => {
       switch (jobs.length) {
         case 1:
       console.log(jobs.length)        
-          addAchievement(1); // badge_id for 1st job app submitted
+          addAchievement(2); // badge_id for 1st job app submitted
           console.log('First Job Application Logged')
           break;
         case 25: // badge_id for 25 job apps submitted
-          addAchievement(2);
+          addAchievement(3);
           console.log('25th Job Application Logged')
           break;
         case 50: // badge_id for 50 job apps submitted
-          addAchievement(3);
+          addAchievement(4);
           console.log('50th Job Application Logged')
           break;
         case 100: // badge_id for 100 job apps submitted
-          addAchievement(4);
+          addAchievement(5);
           console.log('100th Job Application Logged')          
           break;
         case 200: // badge_id for 200 job apps submitted
-          addAchievement(5);
+          addAchievement(6);
           console.log('200th Job Application Logged')          
           break;
         case 300: // badge_id for 300 job apps submitted
-          addAchievement(6);
+          addAchievement(7);
           console.log('300th Job Application Logged')  
           break;
         case 400: // badge_id for 400 job apps submitted
-          addAchievement(7);
+          addAchievement(8);
           console.log('400th Job Application Logged')  
           break;
         case 500: // badge_id for 500 job apps submitted
-          addAchievement(8);
+          addAchievement(9);
           console.log('500th Job Application Logged')  
           break;
         default:
@@ -103,15 +105,15 @@ const checkInterviewNumber = () => {
       switch (interviews.length) {
         case 1:
         console.log('First Interview Logged')
-          addAchievement(9); // badge_id for 1st interview saved
+          addAchievement(10); // badge_id for 1st interview saved
           break;
         case 5: // badge_id for 5 interviews saved
         console.log('5th Interview Logged')
-          addAchievement(10);
+          addAchievement(11);
           break;
         case 10: // badge_id for 10 interviews saved
         console.log('10th Interview Logged')
-          addAchievement(11);
+          addAchievement(12);
           break;
         default:
           console.log('No Achievements earned for interviews saved');
