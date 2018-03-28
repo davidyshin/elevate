76685784ebcee09960414662969f618a6cd326e7
davidyshin <davidshin@ac.c4q.nyc>
2018-03-27 22:10:05 -0400

    made notification settings persist for user oupon page refresh. styled a little too

--- frontend/src/components/user/account/UserInfo.jsx
+++ frontend/src/components/user/account/UserInfo.jsx
@@ -3,28 +3,28 @@ import Modal from 'react-modal';
 import EditUser from './EditUser.jsx';
 import axios from 'axios';
 
-
 class Select extends React.Component {
   render() {
-    const { values, selectedValue, handleSelect } = this.props
-    const displayValues = ['', ...values]
+    const { values, selectedValue, handleSelect, email, phone } = this.props;
+    const displayValues = ['', ...values];
 
     return (
       <select
+        className="select-interval-option"
         value={selectedValue}
         onChange={handleSelect}
+        disabled={!email && !phone}
       >
-        {displayValues.map(val =>
-          <option value={val}>{val}</option>)}
+        {displayValues.map(val => <option value={val}>{val}</option>)}
       </select>
-    )
+    );
   }
 }
 
 class UserInfo extends Component {
   constructor() {
     super();
-    this.interval = [1, 3, 7]
+    this.interval = [1, 3, 7];
 
     this.state = {
       modalOpen: false,
@@ -33,11 +33,18 @@ class UserInfo extends Component {
       notification_interval: 7
     };
   }
+  componentDidMount() {
+    const { activeUser } = this.props;
+    this.setState({
+      email_notification: activeUser.email_notification,
+      phone_notification: activeUser.phone_notification,
+      notification_interval: activeUser.notification_interval
+    });
+  }
 
-
-  // Set notification settings by getting the data from backend 
-  // On checkbox, set state 
-  // On checkbox, update database with Y/N for notification settings 
+  // Set notification settings by getting the data from backend
+  // On checkbox, set state
+  // On checkbox, update database with Y/N for notification settings
 
   toggleModal = () => {
     let { modalOpen } = this.state;
@@ -45,75 +52,106 @@ class UserInfo extends Component {
       modalOpen: !modalOpen
     });
   };
-  
+
   handleReminderSelect = e => {
     this.setState({
-      notification_interval : e.target.value
-
-    })
+      notification_interval: e.target.value
+    });
   };
 
   handleCheckBoxChange = e => {
     this.setState({
       [e.target.name]: e.target.checked
-    })
+    });
   };
 
-
   handleSave = e => {
-    const {notification_interval, email_notification, phone_notification} = this.state
-    axios.put('/users/updateNotification',{ notification_interval, email_notification, phone_notification})
-    .then(res =>{
-      console.log(res)
-    })
-    .catch( err=>{
-      console.log(err)
-    })
-  }
-
-
+    const {
+      notification_interval,
+      email_notification,
+      phone_notification
+    } = this.state;
+    axios
+      .put('/users/updateNotification', {
+        notification_interval,
+        email_notification,
+        phone_notification
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
-    console.log(this.state);
-    const { notification_interval } = this.state;
+    const {
+      notification_interval,
+      email_notification,
+      phone_notification
+    } = this.state;
 
     return (
       <div className="user-account-container" data-aos="fade-up">
         <div className="user-info-container">
-
           <div className="user-info-header">
             <h3>Account Information</h3>
-            <i onClick={this.toggleModal} className="far fa-edit user-info-edit"></i>
+            <i
+              onClick={this.toggleModal}
+              className="far fa-edit user-info-edit"
+            />
           </div>
           <div>
-            <p>Name: {this.props.activeUser.first_name} {this.props.activeUser.last_name}</p>
+            <p>
+              Name: {this.props.activeUser.first_name}{' '}
+              {this.props.activeUser.last_name}
+            </p>
             <p>Email: {this.props.activeUser.username}</p>
             <p>Phone number: {this.props.activeUser.phone_number}</p>
           </div>
-
         </div>
 
         <div className="user-settings-container">
           <h3>Notification Settings</h3>
           <div>
-            <p>Send me an important reminder
+            <p>
+              Send me an important reminder
               <Select
                 values={this.interval}
                 selectedValue={notification_interval}
                 handleSelect={this.handleReminderSelect}
-              /> days before the event.</p>
+                email={email_notification}
+                phone={phone_notification}
+              />{' '}
+              days before any interview.
+            </p>
 
             <p>Notify me on my:</p>
-            <label>
-              <input type="checkbox" name="phone_notification" onChange={this.handleCheckBoxChange} />
+            <label className="phone-notification-label">
+              <input
+                className="phone-notification-checkbox"
+                type="checkbox"
+                checked={phone_notification}
+                name="phone_notification"
+                onChange={this.handleCheckBoxChange}
+              />
               Phone
             </label>
-            <label>
-              <input type="checkbox" name="email_notification" onChange={this.handleCheckBoxChange} />
+            <label className="email-notification-label">
+              <input
+                className="email-notification-checkbox"
+                type="checkbox"
+                checked={email_notification}
+                name="email_notification"
+                onChange={this.handleCheckBoxChange}
+              />
               Email
             </label>
           </div>
-          <button onClick={this.handleSave}>Save</button>
+          <div className="save-button">
+          <button className="user-info-save-button"onClick={this.handleSave}>Save</button>
+          </div>
         </div>
 
         <Modal
