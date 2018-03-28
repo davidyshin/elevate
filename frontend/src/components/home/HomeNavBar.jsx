aa8927fe863b30c7a3ac1f60a172c9803450f5d4
helencho <helencho@ac.c4q.nyc>
2018-03-28 18:33:09 -0400

    animated hamburger menu when browser width is smaller than 720px

--- frontend/src/components/home/HomeNavBar.jsx
+++ frontend/src/components/home/HomeNavBar.jsx
@@ -8,31 +8,76 @@ import axios from 'axios';
 class HomeNavBar extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      expandMenu: false
+    };
+  }
+
+  handleMenuClick = () => {
+    this.setState({
+      expandMenu: !this.state.expandMenu
+    });
+  }
+
+  handleCloseMenu = () => {
+    this.setState({
+      expandMenu: false
+    });
   }
 
   render() {
+    const { expandMenu } = this.state; 
+
+    const expandClass = expandMenu ? 'home-nav-bar-mobile-expand' : null;
+
     return (
-      <div className="home-nav-bar">
-        <div className="home-nav-left">
-          <Link to="/">
-            <img
-              src="https://i.imgur.com/JdYm85w.png"
-              alt="elevate"
-            />
-          </Link>
-          <h3><Link to="/">JOBS</Link></h3>
-          <h3><Link to="/addjob">ADD JOB</Link></h3>
-          <h3><Link to="/leaderboard">LEADERBOARD</Link></h3>        
-          <h3><Link to="/community">COMMUNITY</Link></h3>                      
-        </div>
-        <div className="home-nav-right">
-          <div className="home-nav-exp-container">
-            <img src="https://i.imgur.com/oudBkRW.png" alt="exp-coins" />
-            <p>{this.props.experience}</p>
+      <div>
+        <div className="home-nav-bar">
+          <div className="home-nav-left">
+            <Link to="/">
+              <img
+                src="https://i.imgur.com/JdYm85w.png"
+                alt="elevate"
+              />
+            </Link>
+            <h3><Link to="/">JOBS</Link></h3>
+            <h3><Link to="/addjob">ADD JOB</Link></h3>
+            <h3><Link to="/leaderboard">LEADERBOARD</Link></h3>
+            <h3><Link to="/community">COMMUNITY</Link></h3>
           </div>
-          <Link to="/profile"><i className="far fa-user-circle fa-2x"></i></Link>
-        </div>
+          <div className="home-nav-right">
+            <div className="home-nav-exp-container">
+              <img src="https://i.imgur.com/oudBkRW.png" alt="exp-coins" />
+              <p>{this.props.experience}</p>
+            </div>
+            <Link to="/profile"><i className="far fa-user-circle fa-2x"></i></Link>
+          </div>
+        </div>{/* End nav bar web browser */}
+
+        <div className="home-nav-bar-mobile">
+          <div className="home-nav-bar-mobile-fixed">
+            <div className="home-nav-bar-bars" onClick={this.handleMenuClick}>
+              <i className="fas fa-bars fa-2x"></i>
+            </div>
+
+            <div className="home-nav-bar-mobile-right">
+              <div className="home-nav-exp-container">
+                <img src="https://i.imgur.com/oudBkRW.png" alt="exp-coins" />
+                <p>{this.props.experience}</p>
+              </div>
+              <Link to="/profile"><i className="far fa-user-circle fa-2x"></i></Link>
+            </div>
+          </div>
+
+          <div className={`home-nav-bar-mobile-dropdown ${expandClass}`}>
+            <h3 onClick={this.handleCloseMenu}><Link to="/">JOBS</Link></h3>
+            <h3 onClick={this.handleCloseMenu}><Link to="/addjob">ADD JOB</Link></h3>
+            <h3 onClick={this.handleCloseMenu}><Link to="/leaderboard">LEADERBOARD</Link></h3>
+            <h3 onClick={this.handleCloseMenu}><Link to="/community">COMMUNITY</Link></h3>
+          </div>
+
+        </div>{/* End nav bar mobile */}
+
       </div>
     );
   }
