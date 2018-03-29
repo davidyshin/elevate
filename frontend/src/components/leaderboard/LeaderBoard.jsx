082ba3d3fb50b91b490e259e3ccb6437b092a158
ramborell23 <jerelldavis@ac.c4q.nyc>
2018-03-29 14:55:25 -0400

    Leaderboard| Updated leaderboard to now contain users image

--- frontend/src/components/leaderboard/LeaderBoard.jsx
+++ frontend/src/components/leaderboard/LeaderBoard.jsx
@@ -4,31 +4,6 @@ import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import '../../stylesheets/leaderboard.css';
 
-
-const columns = [{
-  Header: 'Rank',
-  accessor: 'rank', // String-based value accessors!
-  Cell: props => <span className='number'>{(props.index) + 1} </span>,
-  minWidth: 120, // Custom cell components!
-  maxWidth: 220, // Custom cell components!
-  // show:false
-
-}, {
-  id: 'name',
-  Header: 'Name',
-  accessor: d => d.first_name + ' ' + d.last_name, // String-based value accessors!
-  minWidth: 50,// Custom cell components!
-  minWidth: 220,// Custom cell components!
-  // accessor: ''  
-}, {
-  Header: 'Experience',
-  accessor: 'experience',
-  Cell: props => <span className='number'>{props.value} </span>,
-  minWidth: 220,// Custom cell components!
-  sortable: false,
-
-},
-]
 class LeaderBoard extends Component {
   constructor() {
     super();
@@ -37,6 +12,7 @@ class LeaderBoard extends Component {
       top5: []
     }
   }
+
   componentDidMount() {
     axios
       .get('/users/getLeaders')
@@ -50,8 +26,37 @@ class LeaderBoard extends Component {
         console.log(err);
       });
   }
+  
   render() {
-    const { top5 } = this.state;
+    const { top5 , user} = this.state;
+    const columns = [{
+      Header: 'Rank',
+      accessor: 'rank', // String-based value accessors!
+      Cell: props => <span className='number'>{(props.index) + 1} </span>,
+      minWidth: 120, // Custom cell components!
+      maxWidth: 220, // Custom cell components!
+      // show:false
+    
+    }, {
+      id: 'name',
+      Header: 'Name',
+      accessor: d => d.id === user.id ? (<span><img className='avatar' src={user.photo_url} alt='user photo'/>    You </span>) : (<span><img className='avatar' src={d.photo_url} alt='user photo'/>{'    '+d.first_name+ '  '+ d.last_name}</span>), // String-based value accessors!
+      minWidth: 50,// Custom cell components!
+      minWidth: 250,// Custom cell components!
+      // accessor: ''  
+    }, {
+      Header: 'Experience',
+      accessor: 'experience',
+      Cell: props => <span className='number'>{props.value} </span>,
+      minWidth: 120,// Custom cell components!
+      sortable: false,
+    
+    },
+    ]
+console.log(user, top5)
+
+
+
 
     return  top5.length > 4 ? (
       <div className='main' data-aos='fade-up'>
@@ -84,6 +89,7 @@ class LeaderBoard extends Component {
         <br/>
         <br />
       </div>
+      
       </div>
     ) : (<h1> Loading </h1>)
   }
