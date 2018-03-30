a01616ee6f85d74b2c83e010805d900ad8e9b8a1
helencho <helencho@ac.c4q.nyc>
2018-03-30 14:49:29 -0400

    reminder for notification settings popups if it's the user's first interview

---
+++ frontend/src/components/jobs/PopupReminder.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class PopupReminder extends Component {
+  render() {
+    return (
+      <div className="popup-reminder-container">
+        <h3>Don't forget to set your reminder settings!</h3>
+        <p>Profile > Account > Notification Settings</p>
+      </div>
+    )
+  }
+}
+
+export default PopupReminder;
