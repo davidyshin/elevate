e2fe977be4592d281a3540e7caacefa824b44707
helencho <helencho@ac.c4q.nyc>
2018-04-03 09:57:12 -0400

    fix the issue with resizing the log job form page

--- frontend/src/components/jobs/forms/AddJobForm.jsx
+++ frontend/src/components/jobs/forms/AddJobForm.jsx
@@ -274,7 +274,7 @@ class AddJobForm extends Component {
     // console.log('hello');
     e.preventDefault()
     let { applicationStage } = this.state;
-    applicationStage +=1
+    applicationStage += 1
     this.setState({ applicationStage });
   };
 
@@ -304,7 +304,7 @@ class AddJobForm extends Component {
             handleCoverInput={this.handleCoverInput}
             handleSkipButton={this.handleSkipButton}
             job_id={job_id}
-            formStatus={'add'}            
+            formStatus={'add'}
           />
         );
       case 4:
@@ -403,85 +403,86 @@ class AddJobForm extends Component {
             </span>
           </div>
         ) : null} */}
-        <div
-          data-aos="fade-up"
-          hidden={applicationStage > 1 ? true : false}
-          className="add-job-info"
-        >
-          <form onSubmit={this.handleFirstSubmit}>
-            <h1>Job Application Information</h1>
-            <h3>Let's start with the basics</h3>
+        <div hidden={applicationStage > 1 ? true : false}>
+          <div
+            data-aos="fade-up"
+            className="add-job-info"
+          >
+            <form onSubmit={this.handleFirstSubmit}>
+              <h1>Job Application Information</h1>
+              <h3>Let's start with the basics</h3>
 
-            <div className="add-job-input-pairs-container">
-              <div className="add-job-labels-container">
-                <p>Company *</p>
-                <p>Position *</p>
-                <div>
-                  <p className="add-job-date-label">Date applied *</p>
-                </div>
-                <p>Job posting url</p>
-              </div>
-              <div className="add-job-inputs-container">
-                <div className="company-search-input">
-                  <Autosuggest
-                    className="add-job-form-input-company"
-                    theme={AutoSuggestStyling}
-                    suggestions={suggestedCompanies}
-                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                    getSuggestionValue={this.getSuggestionValue}
-                    onSuggestionSelected={this.onSuggestionSelected}
-                    renderSuggestion={this.renderSuggestion}
-                    inputProps={inputProps}
-                  />
-                  {companyLogo ? (
-                    <img className="company-image" src={companyLogo} />
-                  ) : (
-                      <span className="magnifying-glass">
-                        <i className="fas fa-search fa-2x" />
-                      </span>
-                    )}
-                </div>
-                <div className="position-search-input">
+              <div className="add-job-input-pairs-container">
+                <div className="add-job-labels-container">
+                  <p>Company *</p>
+                  <p>Position *</p>
                   <div>
+                    <p className="add-job-date-label">Date applied *</p>
+                  </div>
+                  <p>Job posting url</p>
+                </div>
+                <div className="add-job-inputs-container">
+                  <div className="company-search-input">
+                    <Autosuggest
+                      className="add-job-form-input-company"
+                      theme={AutoSuggestStyling}
+                      suggestions={suggestedCompanies}
+                      onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                      onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                      getSuggestionValue={this.getSuggestionValue}
+                      onSuggestionSelected={this.onSuggestionSelected}
+                      renderSuggestion={this.renderSuggestion}
+                      inputProps={inputProps}
+                    />
+                    {companyLogo ? (
+                      <img className="company-image" src={companyLogo} />
+                    ) : (
+                        <span className="magnifying-glass">
+                          <i className="fas fa-search fa-2x" />
+                        </span>
+                      )}
+                  </div>
+                  <div className="position-search-input">
+                    <div>
+                      <input
+                        onChange={this.handleInput}
+                        value={position}
+                        placeholder="Position"
+                        name="position"
+                        type="text"
+                      />
+                    </div>
+                    <span className="brief-case">
+                      <i className="fas fa-briefcase fa-2x" />
+                    </span>
+                  </div>
+                  <div className="date-applied-input">
+                    <Calendar onChange={this.handleDate} value={date_applied} />
+                  </div>
+                  <div className="job-posting-input">
                     <input
                       onChange={this.handleInput}
-                      value={position}
-                      placeholder="Position"
-                      name="position"
+                      value={url}
+                      placeholder="Url"
+                      name="url"
                       type="text"
                     />
                   </div>
-                  <span className="brief-case">
-                    <i className="fas fa-briefcase fa-2x" />
-                  </span>
-                </div>
-                <div className="date-applied-input">
-                  <Calendar onChange={this.handleDate} value={date_applied} />
-                </div>
-                <div className="job-posting-input">
-                  <input
-                    onChange={this.handleInput}
-                    value={url}
-                    placeholder="Url"
-                    name="url"
-                    type="text"
-                  />
                 </div>
               </div>
-            </div>
-            <div className="add-job-buttons">
-              <input
-                disabled={saved || !company || !position || !date_applied}
-                type="submit"
-                value="Next"
-              />
-            </div>
-          </form>
+              <div className="add-job-buttons">
+                <input
+                  disabled={saved || !company || !position || !date_applied}
+                  type="submit"
+                  value="Next"
+                />
+              </div>
+            </form>
+          </div>
         </div>
         {applicationStage > 1 ? (
           <div>
-            <this.renderJobSideBar/> <this.renderStage />
+            <this.renderJobSideBar /> <this.renderStage />
           </div>
         ) : null}
       </div>
