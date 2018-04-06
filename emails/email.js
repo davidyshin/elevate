b2ff27f31c43a6689598493a5e876f44a871d7e3
helencho <helencho@ac.c4q.nyc>
2018-04-05 21:44:59 -0400

    restyle the welcome email

--- emails/email.js
+++ emails/email.js
@@ -1,13 +1,13 @@
 const welcomeEmail = firstName => `
-<table style="background-color:#283c53; font-family:'Montserrat',sans-serif; color:#fefefe">
+<table style = "background-color:#283c53; font-family:sans-serif; color:#fefefe; border-radius:4px;">
   <tr>
-    <td style="text-align:center;">
-      <p style="font-size:36px;"><img src="https://image.ibb.co/hMyhun/qK34BJA.png" style="height:32px; width:32px;"/>Elevate</p>
+    <td style = "text-align:center;">
+      <p style = "font-size:36px;"><img src="https://image.ibb.co/hMyhun/qK34BJA.png" style="height:32px; width:32px; padding:0 22px;"/>Elevate</p>
     </td>
   </tr>
 
-  <tr>
-    <td style="padding:22px; line-height:1.8;">
+  <tr style="font-size:16px">
+    <td style="padding:22px 32px; line-height:1.8;">
       <p>${firstName},</p>
       <p>Thank you for signing up with Elevate. Now you'll be able to easily organize your job applications and manage your job search process. On top of that, you will:</p>
       
@@ -16,6 +16,9 @@ const welcomeEmail = firstName => `
         <li>Track each job application progress</li>
         <li>Earn points and level up towards your success!</li>
       </ul>
+
+      <p>Go ahead and <a href="http://elevate-ny.herokuapp.com/" target="_blank" style = "color:#3498db; text-decoration:none;">log your first job application.</a> Good luck on your job search journey.</p>
+
       <p>Cheers,</p>
       <p>Team Elevate</p>
     </td>
@@ -23,12 +26,13 @@ const welcomeEmail = firstName => `
 
   <tr>
     <td style="text-align:center; padding:22px;">
-      <a href="#" target="_blank">
-        <img src="https://image.ibb.co/n6dFEn/Git_Hub_Mark_Light_120px_plus.png" style="height:32px; width:32px;"/>
+      <a href="https://github.com/davidyshin/elevate" target="_blank">
+        <img src="https://image.ibb.co/n6dFEn/Git_Hub_Mark_Light_120px_plus.png" style="height:40px; width:40px;"/>
         </a>
     </td>
   </tr>
-</table>`
+</table>
+`
 
 // const welcomeEmail = firstName => `
 // <table style = "background-color:#32BBDA;  width:80%; height:1000px; border-radius: 25px">
