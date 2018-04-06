b701dde447112767e3f3415891ab24eeb935270b
helencho <helencho@ac.c4q.nyc>
2018-04-05 22:24:37 -0400

    remove unused function

--- emails/email.js
+++ emails/email.js
@@ -71,25 +71,6 @@ const reminder = (firstName, company_name, interview_date, interview_time) => `
 </table>
 `;
 
-// const reminder = (firstName, company_name, interview_date, interview_time) =>
-//         `<table style = "background-color:#32BBDA;  width:80%; height:1000px; border-radius: 25px">
-//         <tr>
-//                <td align="center" ><img src="https://image.ibb.co/hMyhun/qK34BJA.png" style="width:5%;  height:25%;" > </td>
-//         </tr>
-//         <tr style = "background-color:#E5F6FA;" >
-//                <td style = "padding:40px; border-radius: 25px" > 
-//                <p> Hello ${firstName}</p>
-//                <p> This is a reminder for your interview with ${company_name} on ${moment(interview_date).format(
-//                 'dddd, MMMM Do YYYY')} at ${moment(interview_time, 'HH:mm').format('hh:mm a')}.<p>
-//                <p> Good Luck! </p>
-//                <p> -Team Elevate </p>
-//               </td>
-//         </tr>
-//         <tr>
-//                <td align="center"><img src="https://image.ibb.co/n6dFEn/Git_Hub_Mark_Light_120px_plus.png" style="width:5%;height:25%;"></td>
-//         </tr>
-// </table>
-// `;
 
 module.exports = {
         welcomeEmail: welcomeEmail,
